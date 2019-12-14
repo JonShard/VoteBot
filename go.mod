bed6a5d24aefced635ee01b423f7b751ade70c47
JonShard <jonems@stud.ntnu.no>
2019-12-14 19:23:37 +0100

    Added #2. HelloWorld handler, #1 Read config file.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module VoteBot
 
 go 1.13
+
+require (
+	github.com/bwmarrin/discordgo v0.20.1
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
+	gopkg.in/yaml.v2 v2.2.7
+)
--- main.go
+++ main.go
@@ -1,10 +1,103 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"gopkg.in/yaml.v2"
 )
+
+// Token contains the authorization token for communicating with discord.
+type config struct {
+	BotToken string `yaml:"botToken"`
+}
+
+// validates the message and splits it into an array of substings split on space.
+func parseMessage(m *discordgo.MessageCreate) ([]string, error) {
+
+	if len(m.Content) < 5 {
+		return nil, errors.New("message too short. No commands exist shorter than 5 chars")
+	}
+
+	substrings := strings.Split(m.Content, " ")
+
+	return substrings, nil
+}
+
+// reads config file from file into struct 'config'.
+func readConfigFile(c *config) *config {
+
+	yamlFile, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		fmt.Printf("yamlFile.Get err #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return c
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func hellowWorldHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	message, err := parseMessage(m)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	if message[0] == "!hello" {
+		s.ChannelMessageSend(m.ChannelID, "World!")
+	}
+}
 
 func main() {
 
-	fmt.Println("Hello world!")
+	// Load environment file:
+	cfg := config{}
+	readConfigFile(&cfg)
+
+	fmt.Println("Loaded config file: \nToken: " + cfg.BotToken)
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + cfg.BotToken)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(hellowWorldHandler)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
 }
